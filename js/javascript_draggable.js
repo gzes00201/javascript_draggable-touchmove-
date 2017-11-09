b355d7d13629e257c570909881112baeb8547bb7
PIN大 <gzes00201@gmail.com>
2017-11-09 13:52:15 +0800

    Update javascript_draggable.js

--- js/javascript_draggable.js
+++ js/javascript_draggable.js
@@ -1,7 +1,21 @@
 //用此方法新增 var touch_move=new js_drag('#items-list > li');
 function js_drag(box_item){
   let items = document.querySelectorAll(box_item)
-  
+  let parent_box_type_tagName = document.querySelectorAll(box_item)[0].parentElement.tagName
+  let parent_box_IdorClass='';
+  if(document.querySelectorAll(box_item)[0].parentElement.getAttribute('id')){
+      parent_box_IdorClass='#'+document.querySelectorAll(box_item)[0].parentElement.getAttribute('id');
+  }else{
+      parent_box_IdorClass='.'+document.querySelectorAll(box_item)[0].parentElement.getAttribute('class');
+  }
+  console.log(parent_box_IdorClass);
+  let this_box_type_tagName = document.querySelectorAll(box_item)[0].tagName
+  let this_box_IdorClass='';
+  if(document.querySelectorAll(box_item)[0].getAttribute('id')){
+      this_box_IdorClass='#'+document.querySelectorAll(box_item)[0].getAttribute('id');
+  }else{
+      this_box_IdorClass='.'+document.querySelectorAll(box_item)[0].getAttribute('class');
+  }
   items.forEach(item => {
 
     item.setAttribute('draggable', true)
@@ -10,6 +24,12 @@ function js_drag(box_item){
     item.addEventListener('drop', dropped)
     item.addEventListener('dragenter', cancelDefault)
     item.addEventListener('dragover', cancelDefault)
+
+    item.addEventListener("touchstart", dragStart);
+    item.addEventListener("touchend", dropped);
+    item.addEventListener("touchcancel", cancelDefault);
+    item.addEventListener("touchleave", dropped);
+    item.addEventListener("touchmove", cancelDefault);
   })
 
   function get_index (box,item) {
@@ -19,19 +39,30 @@ function js_drag(box_item){
   }
 
   function dragStart (e) {
-
-    var index = get_index(document.getElementById('items-list'),e.target);
-    e.dataTransfer.setData('text/plain', index)
-
+    let this_dom=get_real_enm(e.target);
+    if(this_dom){
+        let index = get_index(document.getElementById('items-list'),this_dom);
+        e.dataTransfer.setData('text/plain', index)
+    }
   }
-
+    function get_real_enm (this_dom) {
+        while (this_dom.tagName!=this_box_type_tagName && !('#'+this_dom.getAttribute('id')==parent_box_IdorClass || '.'+document.querySelectorAll(box_item)[0].parentElement.getAttribute('class')==parent_box_IdorClass) && this_dom){
+            //尋找正確的移動物件
+            this_dom=this_dom.parentElement;
+        }
+        if(this_dom){
+            return this_dom;
+        }else{
+            return null;
+        }
+    }
   function dropped (e) {
     cancelDefault(e)
-    
+    console.log(e.dataTransfer);
     // get new and old index
     let oldIndex = e.dataTransfer.getData('text/plain')
     console.log(oldIndex);
-    let target = e.target
+    let target = get_real_enm(e.target);
     console.log(target);
     let newIndex = get_index(document.getElementById('items-list'),target);
     console.log(newIndex);
